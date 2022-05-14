a22b6658cdbba2e6341cc183a7f7eadbad190358
Enoughdv <mxhrsas@gmail.com>
2022-05-14 00:05:15 -0500

    update

--- src/main/java/me/ichoco/prison/utils/LocationUtil.java
+++ src/main/java/me/ichoco/prison/utils/LocationUtil.java
@@ -4,11 +4,10 @@ import lombok.experimental.UtilityClass;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
 
 @UtilityClass
 public class LocationUtil {
-    
+
     public static String parseToString(Location location) {
         return (location == null) ? null : location.getX() + ", " + location.getY() + ", " + location.getZ() + ", " + location.getYaw() + ", " + location.getPitch() + ", " + location.getWorld().getName();
     }
@@ -19,13 +18,11 @@ public class LocationUtil {
         }
         String[] data = string.split(", ");
         try {
-            double x = Double.parseDouble(data[0]);
-            double y = Double.parseDouble(data[1]);
-            double z = Double.parseDouble(data[2]);
-            float pitch = Float.valueOf(data[4]);
-            float yaw = Float.valueOf(data[3]);
-            World world = Bukkit.getWorld(data[5]);
-            Location location = new Location(world, x, y, z, yaw, pitch);
+
+            Location location = new Location(Bukkit.getWorld(data[5]), Double.parseDouble(data[0]),
+                    Double.parseDouble(data[1]), Double.parseDouble(data[2]),
+                    Float.valueOf(data[3]), Float.valueOf(data[4]));
+
             return location;
         } catch (NumberFormatException e) {
             e.printStackTrace();
